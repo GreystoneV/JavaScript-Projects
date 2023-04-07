ee5c8d7cbebcfe1806e2c80ba318de37157e8e91
GreystoneV <benjaminflevi@gmail.com>
2023-04-07 08:47:37 -0500

    Make additional function.

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -1,7 +1,7 @@
 //This function is called in our html when the user clicks the button. It replaced the button text with the cariable str. The text hello world gets replaced by the variable i which is the value at index 0 of the string str. 
 
 //Im not sure if this is what you wanted in the assignment that said to initilize 2 variables and to use a previous function. 
-function My_First_Function() {
+function myFirstFunction() {
     var str = "This is the button text!";
     var i = str[0];
     console.log(i);
@@ -13,4 +13,15 @@ function myFunction() {
     var sentence = "I am learning";
     sentence += " a lot from this book!";
     document.getElementById("Concatenate").innerHTML = sentence;
+}
+
+//Stores color of text element with id Concatenate, stores the element, checks if the color is equal to green, if so, changes color to blue.
+// Step 75 challenge
+function changeColor() {
+    colorCheck = document.getElementById("Concatenate").style.color;
+    targetEle = document.getElementById("Concatenate").innerHTML;
+    if (colorCheck === 'green') {
+        colorCheck = targetEle.fontcolor("blue");
+        document.getElementById('Concatenate').innerHTML = colorCheck;
+    }
 }
