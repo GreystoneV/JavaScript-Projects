a3fd3abfa90cb81eba29aca6fc9e6a1a79f7397e
GreystoneV <benjaminflevi@gmail.com>
2023-04-07 17:58:16 -0500

    make time func use if else

--- Basic-JavaScript-Projects/Project7_scope_time_function/JS/main.js
+++ Basic-JavaScript-Projects/Project7_scope_time_function/JS/main.js
@@ -11,3 +11,17 @@ function something() {
 document.write(x);
 // call function something
 something();
+
+// Step 146 make function to check time and return what the user should do if it is past 7 pm, uses if else
+function check() {
+    yee = new Date().getHours();
+    if (yee < 19) {
+        document.getElementById('time').innerHTML = "Study!";
+    }
+    else if (yee > 19) {
+        document.getElementById('time').innerHTML = "Sleep!";
+    }
+    else {
+        document.getElementById('time').innerHTML = "Something went so very very wrong!"
+    }
+}
