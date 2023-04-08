6babaa81dce2f54cdf18244c7b159db3f9bd6a9d
GreystoneV <benjaminflevi@gmail.com>
2023-04-08 07:10:21 -0500

    Add img, work on bug.

--- Basic-JavaScript-Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic-JavaScript-Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,21 +1,3 @@
-//  Step 169
-// Function creats a timer and displays a countdown based on how many seconds the user inputs
-function countdown() {
-    var seconds = document.getElementById("seconds").value;
-
-    function tick() {
-        seconds = seconds - 1;
-        timer.innerHTML = seconds;
-        var time = setTimeout(tick, 1000);
-        if (seconds == -1) {
-            alert("Time's up!");
-            clearTimeout(time);
-            timer.innerHTML = '';
-        }
-    }
-    tick();
-}
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
