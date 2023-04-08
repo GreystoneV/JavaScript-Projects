ea2c9a7bc3c86396cf4ee795cbe8e26f3264841d
GreystoneV <benjaminflevi@gmail.com>
2023-04-08 11:55:50 -0500

    make gradient

--- Basic-JavaScript-Projects/Basic_JS_11/JS/main.js
+++ Basic-JavaScript-Projects/Basic_JS_11/JS/main.js
@@ -5,3 +5,22 @@ function change() {
     x[0].innerHTML = "We are many.";
     x[1].innerHTML = "We are few.";
 }
+
+// Step 239
+function draw() {
+    // set our config variables
+    canvas = document.getElementById('Pic');
+    ctx = canvas.getContext('2d');
+
+    // outlined square X: 50, Y: 35, width/height 50
+    ctx.beginPath();
+    ctx.strokeRect(100, 100, 50, 50);
+
+    // filled square X: 125, Y: 35, width/height 50
+    var grd = ctx.createLinearGradient(0, 0, 170, 0);
+    grd.addColorStop(0, "black");
+    grd.addColorStop(1, "white")
+    ctx.beginPath();
+    ctx.fillStyle = grd;
+    ctx.fillRect(20, 20, 150, 100);
+}
