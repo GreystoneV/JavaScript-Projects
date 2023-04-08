ca07522f3c24d4539709059fae0bab9ae8835864
GreystoneV <benjaminflevi@gmail.com>
2023-04-08 09:29:11 -0500

    impliment a while loop

---
+++ Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,12 @@
+// Step 197
+// Function writes the value of x to different paragraphs. The value of the id incriments along with x.
+function callLoop() {
+    let x = "1";
+    let idVar = "";
+    while (x < 6) {
+        idVar = x;
+        document.getElementById(idVar).innerHTML = x;
+        x++;
+    }
+
+}
