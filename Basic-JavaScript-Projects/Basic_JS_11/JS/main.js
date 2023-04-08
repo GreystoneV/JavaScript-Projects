4683640cb972a71b14b4de0ae5505ac4605d2c29
GreystoneV <benjaminflevi@gmail.com>
2023-04-08 11:40:20 -0500

    use classname and slicing

---
+++ Basic-JavaScript-Projects/Basic_JS_11/JS/main.js
@@ -0,0 +1,7 @@
+// Step 235
+// function gets information by class and then changes it based on the given index
+function change() {
+    var x = document.getElementsByClassName("test");
+    x[0].innerHTML = "We are many.";
+    x[1].innerHTML = "We are few.";
+}
