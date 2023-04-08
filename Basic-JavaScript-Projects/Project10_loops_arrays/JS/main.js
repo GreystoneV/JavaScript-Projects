072aee24b6c5a39833ef58d80509d0fba6564da9
GreystoneV <benjaminflevi@gmail.com>
2023-04-08 10:00:28 -0500

    create array

--- Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
+++ Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
@@ -10,8 +10,34 @@ function callLoop() {
     }
 
 }
-
+// finds length of x and displays it
 function callLength() {
     let x = "Hello Friend!";
     document.getElementById("length").innerHTML += x.length;
+}
+
+// Step 200
+// I wonder if JS has list comprehension?
+var Instruments = ["Banjo", "Drums", "Battle-Axe"];
+var Content = "";
+var Y;
+// It's weird to not have to pass in variables to the functions. Is this normal? I'm used to having to declare a variable global.
+// Doesn't having every variable outside functions be global, create issues? Or do you just have to be more careful with naming conventions? 
+function forLoop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("listOfInstruments").innerHTML = Content;
+}
+
+// Step 205
+// Initilize array animals, set values at various index, 
+// Interesting that you can skip an index and it will return undefined
+var animals = [];
+animals[0] = "Cat";
+animals[1] = "Dog";
+animals[3] = "Fish";
+
+function arrayFunction() {
+    document.getElementById("array").innerHTML = animals;
 }
